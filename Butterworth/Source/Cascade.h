ff9450b8894b3b9ffc2dc09857612bd1e253d56c
four-of-seven <stan.barvinsky@gmail.com>
2020-12-29 23:41:10 +0100

    added order enum in cascade

--- Butterworth/Source/Cascade.h
+++ Butterworth/Source/Cascade.h
@@ -15,6 +15,8 @@
 
 namespace syfo
 {
+    enum class Order {dB12, dB16, dB18, dB24, dB36, dB72};
+    
     template <typename T>
     class Cascade
     {
@@ -23,8 +25,12 @@ namespace syfo
         {}
         ~Cascade()
         {}
+        T process (const T& sample)
+        {
+            
+        }
     private:
-        int order;
+        Order order {Order::dB12};
         JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Cascade)
         
     };
